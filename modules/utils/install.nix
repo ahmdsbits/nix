5672842d546160f1b6e3c120341499c9f8b53648
Ahmed Shahir Samin <ahmdsphere+git@gmail.com>
2026-05-31 15:47:49 +0600

    Automatically infer current flake in install script

--- modules/utils/install.nix
+++ modules/utils/install.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ self, config, ... }: {
   perSystem = { pkgs, ... }: {
     apps.nixos-install = {
       type = "app";
@@ -7,13 +7,24 @@
         public-keys = builtins.concatStringsSep " " config.shared.substituters.trusted-public-keys;
         script = pkgs.writeShellScriptBin "nixos-install" ''
           set -e
+          
+          if [[ "$1" == "--flake" ]]; then
+            if [[ -z "$2" ]]; then
+              echo "Error: --flake requires an argument."
+              exit 1
+            fi
+            TARGET_FLAKE="$2"
+          else
+            TARGET_HOSTNAME="$1"
+            TARGET_FLAKE="${self}#$TARGET_HOSTNAME"
+          fi
 
           sudo nixos-install \
             --option extra-substituters "${substituters}" \
             --option extra-trusted-public-keys "${public-keys}" \
             --keep-going \
             --no-root-password \
-            --flake $1
+            --flake "$TARGET_FLAKE"
         '';
       in "${script}/bin/nixos-install";
     };
